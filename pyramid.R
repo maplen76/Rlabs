7ab5c3b7c5bae4de1ea671fb0a164aa2481ec053
Alex <jingwang512@gmail.com>
2018-12-24 09:48:40 +0800

    Create pyramid.R

---
+++ pyramid.R
@@ -0,0 +1,7 @@
+par(mar = c(0, 1, 0, 1))
+pie(
+  c(280, 50, 30),
+  c('Sky', 'Sunny side of pyramid', 'Shady side of pyramid'),
+  col = c('#0292D8', '#F7EA39', '#C4B632'),
+  init.angle = -50, border = NA
+)
